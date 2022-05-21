6ea224b07fd8f5da58211ab0326370a76e946725
ohsuyuki <papu8osu@gmail.com>
2022-05-21 20:42:38 +0900

    Initial Commit

---
+++ GitHubApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubApp
+//
+//  Created by yuuki oosu on 2022/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
